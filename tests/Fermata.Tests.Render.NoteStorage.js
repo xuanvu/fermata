c0f5ed9178225d336ecbdc58c96199e006297974
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-03 23:45:46 +0100

    writing tests for noteStorage.checkStaff

--- tests/Fermata.Tests.Render.NoteStorage.js
+++ tests/Fermata.Tests.Render.NoteStorage.js
@@ -31,6 +31,86 @@ if (typeof require !== 'undefined') {
       });
     });
     
+    describe("#checkStaff", function () {
+      it("existing staff", function () {
+        // given
+        var noteStorage = new NoteStorage();
+        var note = {};
+        var voice = 1;
+        var staff = 1;
+        
+        noteStorage.store(note, voice, staff);
+
+        // when
+        var staffExist = noteStorage.checkStaff(staff);
+        
+        // then
+        assert.ok(staffExist, "staff should exist");
+      });
+      
+      it("no staff", function () {
+        // given
+        var noteStorage = new NoteStorage();
+        var staff = 1;
+
+        // when
+        var staffExist = noteStorage.checkStaff(staff);
+        
+        // then
+        assert.ok(!staffExist, "staff shouldn't exist");
+      });
+      
+      it("after clear", function () {
+        // given
+        var noteStorage = new NoteStorage();
+        var note = {};
+        var voice = 1;
+        var staff = 1;
+        
+        noteStorage.store(note, voice, staff);
+        noteStorage.clear();
+         
+        // when
+        var staffExist = noteStorage.checkStaff(staff);
+        
+        // then
+        assert.ok(!staffExist, "staff shouldn't exist");
+      });
+      
+      it("store by string", function () {
+        // given
+        var noteStorage = new NoteStorage();
+        var note = {};
+        var voice = 1;
+        var staff = 1;
+        
+        noteStorage.store(note, voice, staff.toString());
+         
+        // when
+        var staffExist = noteStorage.checkStaff(staff);
+        
+        // then
+        assert.ok(staffExist, "staff should exist");
+      });
+      
+      it("retrieve by string", function () {
+        // given
+        var noteStorage = new NoteStorage();
+        var note = {};
+        var voice = 1;
+        var staff = 1;
+        
+        noteStorage.store(note, voice, staff.toString());
+         
+        // when
+        var staffExist = noteStorage.checkStaff(staff.toString());
+        
+        // then
+        assert.ok(staffExist, "staff should exist");
+      });
+      
+    });
+    
     describe("#store", function () {
       it("store single note", function () {
         // given
