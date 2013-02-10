954f8d266fec59c6a92e99858d71456953f778ad
Vincent Giersch <vincent@giersch.fr>
2013-02-10 16:13:23 +0000

    Clean Fermata.Mapping.Connector

--- src/Fermata.Mapping.Connector.js
+++ src/Fermata.Mapping.Connector.js
@@ -3,18 +3,18 @@ Fermata.Mapping =      Fermata.Mapping || {};
 Fermata.Mapping.Connector = {};
 
 (function () {
-	"use strict";
+  "use strict";
 
-	Fermata.Mapping.Connector.MusicXmlToVexflow = {
-		"single" : 1,
-		"double" : 2,
-		"brace" : 3,
-		"bracket" : 4
-	};
+  Fermata.Mapping.Connector.MusicXmlToVexflow = {
+    "single" : 1,
+    "double" : 2,
+    "brace" : 3,
+    "bracket" : 4
+  };
 
-	Fermata.Mapping.Connector.getVexflow = function (musicXML) 
-	{
-		return Fermata.Mapping.Connector.MusicXmlToVexflow[musicXML];
-	};
+  Fermata.Mapping.Connector.getVexflow = function (musicXML)
+  {
+    return Fermata.Mapping.Connector.MusicXmlToVexflow[musicXML];
+  };
 
 }).call(this);
