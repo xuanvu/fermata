484ade2f26bd141d1927516a2a4eada61b92e85c
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-16 17:19:27 +0100

    adding method to know if a note has beam

--- Fermata.Render.BeamProcessor.js
+++ Fermata.Render.BeamProcessor.js
@@ -1,10 +1,16 @@
 (function () {
   "use strict";
 
-  Fermata.Render.BeamProcessor= function ()
-  {};
+  Fermata.Render.BeamProcessor= function ($fermata)
+  {
+    this.$fermata = $fermata;
+  };
   
   var BeamProcessor = Fermata.Render.BeamProcessor;
   
+  BeamProcessor.hasBeam = function (note)
+  {
+    return typeof(note.beam) != "undefined";
+  }
   
 }).call(this);
