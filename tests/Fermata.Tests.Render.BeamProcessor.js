b9bfebc134c9d43f8803026bbd3be370d2dba3d1
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-17 10:37:18 +0100

    adding tests

--- tests/Fermata.Tests.Render.BeamProcessor.js
+++ tests/Fermata.Tests.Render.BeamProcessor.js
@@ -1,7 +1,7 @@
 if (typeof require !== 'undefined') {
   var Fermata,
-      fs = require('fs'),
-      assert = require('assert');
+  fs = require('fs'),
+  assert = require('assert');
 
   // Use Scons build version or devel version (using node vm)
   if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
@@ -42,6 +42,48 @@ if (typeof require !== 'undefined') {
       });
     });
     
+    describe("#getBeamType", function (){
+      var tryValue = function (expected)
+      {
+        // Given
+        var beam = {};
+        beam.content = expected;
+
+        // When
+        var actual = BeamProcessor.getBeamType(beam);
+
+        // Then
+        assert.strictEqual(actual, expected);
+      };
+      
+      it("begin", function (){
+        tryValue(BeamType.BEGIN);
+      });
+      
+      it("continue", function (){
+        tryValue(BeamType.CONTINUE);
+      });
+      
+      it("end", function (){
+        tryValue(BeamType.END);
+      });
+      
+      it("failure", function (){
+
+        // Given
+        var beam = {};
+        beam.content = "bad value";
+
+        // Then
+        assert.throws(
+          function() {
+            BeamProcessor.getBeamType(beam);
+          },
+          function(err) {
+            return (err instanceof Error) && /bad value/.test(err);
+          }
+          );
+      });
+    });
   });
-  
 }).call(this);
