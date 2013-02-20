f782df93423a212b7195c58f038a85fdb86e53ee
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-02-20 03:27:19 +0100

    writting test for the stem processor

---
+++ tests/Fermata.Test.Render.StemProcessor.js
@@ -0,0 +1,29 @@
+if (typeof require !== 'undefined') {
+  var Fermata,
+  fs = require('fs'),
+  assert = require('assert');
+
+  // Use Scons build version or devel version (using node vm)
+  if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
+    Fermata = require('..');
+  }
+  else {
+    Fermata = require((process.env['FERMATA_COV'] ? '../src-cov' : '../src') + '/Fermata.Dev.Node.js');
+  }
+
+  // Test utils
+  Fermata.Tests = require('./Fermata.Tests.Utils.js').Tests;
+}
+
+(function(){
+  var StemProcessor = Fermata.Render.StemProcessor;
+  var StemType = Fermata.Render.StemType;
+  
+  describe("Fermata.Render.StemProcessor", function () {
+    describe("#constructor", function (){
+      it("test of the constructor", function (){
+        var stemProcessor = new StemProcessor();
+      });
+    });
+  });
+}).call(this);
