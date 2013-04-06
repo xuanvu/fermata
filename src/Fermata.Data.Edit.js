cdf3e3c58a17485731c89e882b2b06bcaf6f7aee
Vincent Giersch <vincent@giersch.fr>
2013-04-06 17:49:00 +0100

    Edition: add measure(s)

---
+++ src/Fermata.Data.Edit.js
@@ -0,0 +1,31 @@
+(function () {
+  "use strict";
+
+  Fermata.Data.prototype.addMeasure = function(idx, number) {
+    console.log(this);
+
+    if (number === undefined) {
+      number = 1;
+    }
+
+    this.forEachPart(function(part) {
+      if (idx > part.measure.length) {
+        idx = part.measure.length;
+      }
+
+      for (var i = 0 ; i < number ; i++) {
+        part.measure.splice(idx, 0, {'$number': idx + number,'note':[]});
+      }
+
+      if (idx + i > part.measure.length) {
+        part.measure[part.measure.length - 1].barline = {
+          'bar-style': 'light-heavy',
+          '$location': 'right'
+        };
+      }
+
+      console.log(part);
+    });
+  };
+
+}).call(this);
