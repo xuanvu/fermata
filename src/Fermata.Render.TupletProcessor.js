c0a0ec4debad4d1f8f86e5fa2663271a61132f1d
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-04-28 17:03:11 +0200

    creating base file for TupletProcessor

---
+++ src/Fermata.Render.TupletProcessor.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function () {
+  "use strict";
+
+  Fermata.Render.TupletProcessor = function ()
+  {
+    
+  }
+
+  // includes
+  var TupletProcessor = Fermata.Render.TupletProcessor;
+}).call(this);
