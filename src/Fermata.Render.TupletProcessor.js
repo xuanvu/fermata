459623a94b7da67b60bf1b12ca2ed44fb62d6685
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-04-29 02:29:56 +0200

    code for hasTuplet

--- src/Fermata.Render.TupletProcessor.js
+++ src/Fermata.Render.TupletProcessor.js
@@ -10,8 +10,33 @@
   Fermata.Render.TupletProcessor = function ()
   {
     
-  }
+  };
 
   // includes
   var TupletProcessor = Fermata.Render.TupletProcessor;
+  
+  TupletProcessor.hasTuplet = function (note)
+  {
+    if (typeof note.notations === "undefined")
+    {
+      return false;
+    }
+    else if (note.notations instanceof [].constructor)
+    {
+      for (var i = 0 ; i < note.notations.length ; i++)
+      {
+        var notation = note.notations[i];
+            
+        if (typeof notation.tuplet !== "undefined")
+        {
+          return true;
+        }
+      }
+      return false;
+    }
+    else
+    {
+      return typeof note.notations.tuplet !== "undefined";
+    }
+  };
 }).call(this);
