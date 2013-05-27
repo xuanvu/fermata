e1e03a9b06320df1227178ac6cf63fd212a3daf4
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-05-27 03:55:32 +0200

    code for PitchEncapsulator

--- src/Fermata.Render.PitchEncapsulator.js
+++ src/Fermata.Render.PitchEncapsulator.js
@@ -6,5 +6,24 @@
   var SoundType = Fermata.Render.SoundType;
   var PitchPitched = Fermata.Render.PitchPitched;
   var PitchRest = Fermata.Render.PitchRest;
+  
+  Fermata.Render.PitchEncapsulator = {};
+  
+  var PitchEncapsulator = Fermata.Render.PitchEncapsulator
+  
+  PitchEncapsulator.encapsulate = function (noteData)
+  {
+    var soundType = SoundType.getSoundType(noteData);
+
+    if (soundType === SoundType.PITCH) {
+      return new PitchPitched(noteData);
+    }
+    else if (soundType === SoundType.REST) {
+      return new PitchRest(noteData, this.clefName);
+    }
+    else {
+      return null;
+    }
+  };
 
 }).call(this);
