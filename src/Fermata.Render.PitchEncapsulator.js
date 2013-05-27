bd93f51ea2ec1fef0e58f1851b143aad3336e1d8
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-05-27 03:52:12 +0200

    adding includes

--- src/Fermata.Render.PitchEncapsulator.js
+++ src/Fermata.Render.PitchEncapsulator.js
@@ -2,5 +2,9 @@
   "use strict";
 
 
+  // Includes
+  var SoundType = Fermata.Render.SoundType;
+  var PitchPitched = Fermata.Render.PitchPitched;
+  var PitchRest = Fermata.Render.PitchRest;
 
 }).call(this);
