39398eb91cf487e19295b678f67aee3ae4a6bc6d
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-05-30 20:41:59 +0200

    adding base code for unit tests

---
+++ tests/Fermata.Tests.Data.Edit.Measure.js
@@ -0,0 +1,28 @@
+if (typeof require !== 'undefined') {
+  var Fermata,
+  fs = require('fs'),
+  assert = require('assert');
+
+  // Use Scons build version or devel version (using node vm)
+  if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
+    Fermata = require('..');
+  }
+  else {
+    Fermata = require((process.env['FERMATA_COV'] ? '../src-cov' : '../src') + '/Fermata.Dev.Node.js');
+  }
+
+  // Test utils
+  Fermata.Tests = require('./Fermata.Tests.Utils.js').Tests;
+}
+
+(function(){
+  var Data = Fermata.Data;
+   
+  describe("Fermata.Edit.Measure", function () {
+    describe("#addMeasure", function (){
+      it("add at the begining of the measure", function (){
+          
+        });
+    });
+  });
+}).call(this);
