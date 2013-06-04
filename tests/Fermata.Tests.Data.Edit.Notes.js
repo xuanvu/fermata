23a0e2fa76267a2dba694381ab116601da3bd935
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-06-05 00:45:49 +0200

    first tests for change pitch

---
+++ tests/Fermata.Tests.Data.Edit.Notes.js
@@ -0,0 +1,45 @@
+if (typeof require !== 'undefined') {
+  var Fermata,
+  fs = require('fs'),
+  assert = require('assert');
+
+  // Use Scons build version or devel version (using node vm)
+  if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
+    Fermata = require('..');
+  }
+  else {
+    Fermata = require((process.env['FERMATA_COV'] ? '../src-cov' : '../src') + '/Fermata.Dev.Node.js');
+  }
+
+  // Test utils
+  Fermata.Tests = require('./Fermata.Tests.Utils.js').Tests;
+}
+
+(function(){
+  var Data = Fermata.Data;
+  var Utils = Fermata.Utils;
+
+  describe("Fermata.Edit.Note", function () {
+    describe("#changePitch", function (){
+      it("incerate pitched note", function () {
+        var underTest = null;
+        
+        // Given 
+        var pitchedNote = {
+          pitch: {
+            step: "C",
+            octave: 4
+          }
+        };
+        var pitchChange = 1;
+        
+        // When
+        underTest.changePitch(pitchChange);
+        
+        // Then
+        assert.equal(pitchedNote.pitch.step, "D");
+        assert.equal(pitchedNote.pitch.octave, 4);        
+      });
+    });
+  });
+}).call(this);
