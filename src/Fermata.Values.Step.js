6c9169445f0b0d1c4d9491cad7de1837936f84c3
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-06-13 23:42:47 +0200

    fill data for Step

--- src/Fermata.Values.Step.js
+++ src/Fermata.Values.Step.js
@@ -2,5 +2,18 @@
 Fermata.Values.Step = {};
 
 (function(){
+  var Step = Fermata.Values.Step;
 
+  Step.values = ["A", "B", "C", "D", "E", "F", "G"];
+  Step.idx = {};
+  
+  var fillIdx = function (values, idx) {
+    for (var i = 0 ; i < values.length ; i++) {
+      var value = values[i];
+      
+      idx[value] = i;
+    }
+  }
+
+  fillIdx(Step.values, Step.idx);
 }).call(this);
