1edc4ff6e5e5aa48c36fb4c7992dec457124efd5
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-06-13 22:50:22 +0200

    file for PitchRest tests

---
+++ tests/Fermata.Tests.Data.Edit.PitchRest.js
@@ -0,0 +1,19 @@
+if (typeof require !== 'undefined') {
+  var Fermata,
+  fs = require('fs'),
+  assert = require('assert');
+
+  // Use Scons build version or devel version (using node vm)
+  if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
+    Fermata = require('..');
+  }
+  else {
+    Fermata = require((process.env['FERMATA_COV'] ? '../src-cov' : '../src') + '/Fermata.Dev.Node.js');
+  }
+
+  // Test utils
+  Fermata.Tests = require('./Fermata.Tests.Utils.js').Tests;
+}
+
+(function(){
+  }).call(this);
