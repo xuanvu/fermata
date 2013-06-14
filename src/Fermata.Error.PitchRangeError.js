2115b3614a08dea7d7f645d2de4acf1dae63397d
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-06-14 22:49:44 +0200

    adding some exceptions for pitch range error

---
+++ src/Fermata.Error.PitchRangeError.js
@@ -0,0 +1,11 @@
+(function () {
+Fermata.Error.PitchRangeError = function (message) {
+  this.name = "PitchRangeError";
+  this.message = message || "The pitch setted is outside authorized range";
+}
+
+  var PitchRangeError = Fermata.Error.PitchRangeError;
+
+  PitchRangeError.prototype = new Error();
+  PitchRangeError.prototype.constructor = PitchRangeError;
+}).call(this);
---
+++ src/Fermata.Error.StepRangeError.js
@@ -0,0 +1,14 @@
+(function () {
+  var PitchRangeError = Fermata.Error.PitchRangeError;
+
+  Fermata.Error.StepRangeError = function (step) {
+    this.name = "StepRangeError";
+    this.step = step;
+    this.message = "step value " + step + " is outside authorized range";
+  }
+
+  var StepRangeError = Fermata.Error.StepRangeError;
+
+  StepRangeError.prototype = new PitchRangeError();
+  StepRangeError.prototype.constructor = StepRangeError;
+}).call(this);
