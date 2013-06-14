f521fad374da3ef71e8d5fa8caaabf06719636db
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-06-14 22:52:44 +0200

    adding function to check if a step is valid

--- src/Fermata.Values.Step.js
+++ src/Fermata.Values.Step.js
@@ -6,7 +6,10 @@ Fermata.Values.Step = {};
 
   Step.values = ["A", "B", "C", "D", "E", "F", "G"];
   Step.idx = {};
-  
+  Step.check = function (step) {
+    return (!step in Step.idx);
+  };
+
   var fillIdx = function (values, idx) {
     for (var i = 0 ; i < values.length ; i++) {
       var value = values[i];
