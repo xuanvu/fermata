1e087ae86987d0ec08dc77b5da94b4164e43e811
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-06-30 01:53:27 +0200

    base file for measure encapsulation

---
+++ src/Fermata.Data.Measure.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+ Fermata.Data.Measure = function (measureData)
+  {
+    this.data = measureData;
+  };
+
+  var Measure = Fermata.Data.Measure;
+
+}).call(this);
