9ccaa7480a88956f48b3412a89b939878e27c469
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-02 00:18:28 +0200

    test for increased beats

--- tests/Fermata.Tests.Data.Measure.js
+++ tests/Fermata.Tests.Data.Measure.js
@@ -2,7 +2,6 @@ if (typeof require !== 'undefined') {
   var Fermata,
           fs = require('fs'),
           assert = require('assert');
-
   // Use Scons build version or devel version (using node vm)
   if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
     Fermata = require('..');
@@ -12,7 +11,7 @@ if (typeof require !== 'undefined') {
             '/Fermata.Dev.Node.js');
   }
 
-  // Test utils
+// Test utils
   Fermata.Tests = require('./Fermata.Tests.Utils.js').Tests;
 }
 
@@ -23,10 +22,73 @@ if (typeof require !== 'undefined') {
   var BeatsValueError = Fermata.Error.BeatsValueError;
 
 
+  var getTestData = function (nbNote, nbRest) {
+    var measure = {
+      attributes: {
+        divisions: 1,
+        time: {
+          beats: nbNote + nbRest,
+          "beat-type": 4
+        }
+      },
+      note: [],
+      $fermata: {
+        attributes: {
+          divisions: 1,
+          time: {
+            beats: nbNote + nbRest,
+            "beat-type": 4
+          }
+        }
+      }
+    };
+    var i = 0;
+    for (i = 0; i < nbNote; i++) {
+      var note = {
+        duration: 1,
+        pitch: {
+          sign: "C",
+          line: 4
+        }
+      };
+      measure.note.push(note);
+    }
+    for (i = 0; i < nbRest; i++) {
+      var rest = {duration: 1,
+        rest: {}
+      };
+      measure.note.push(note);
+    }
+
+    return measure;
+  };
+
+  var measureToCharTab = function (measure) {
+    var notes = [];
+    for (var i = 0; i < measure.note.length; i++) {
+      var note = measure.note[i];
+      var char = noteToChar(note);
+
+      notes.push(char);
+    }
+
+    return notes;
+  };
+
+  var noteToChar = function (note) {
+    if (typeof note.pitch !== "undefined") {
+      return "p";
+    } else if (typeof note.rest !== "undefined") {
+      return "r";
+    } else {
+      throw new Error("note type not supported");
+    }
+  };
+
   describe("Fermata.Data.Measure", function () {
     describe("#initBeat", function () {
       it("basic value", function () {
-        // Given 
+// Given 
         var beats = 4;
         var beatType = 4;
         var data = {};
@@ -41,7 +103,7 @@ if (typeof require !== 'undefined') {
       });
 
       it("bad beats value: string", function () {
-        // Given 
+// Given 
         var beats = "badValue";
         var beatType = 4;
         var data = {};
@@ -55,7 +117,7 @@ if (typeof require !== 'undefined') {
       });
 
       it("bad beats value: negative", function () {
-        // Given 
+// Given 
         var beats = -5;
         var beatType = 4;
         var data = {};
@@ -69,7 +131,7 @@ if (typeof require !== 'undefined') {
       });
 
       it("bad beats value: null", function () {
-        // Given 
+// Given 
         var beats = 0;
         var beatType = 4;
         var data = {};
@@ -83,7 +145,7 @@ if (typeof require !== 'undefined') {
       });
 
       it("bad beats value: float", function () {
-        // Given 
+// Given 
         var beats = 1.8;
         var beatType = 4;
         var data = {};
@@ -97,7 +159,7 @@ if (typeof require !== 'undefined') {
       });
 
       it("bad beat-type value: non power of 2", function () {
-        // Given 
+// Given 
         var beats = 4;
         var beatType = 5;
         var data = {};
@@ -108,6 +170,25 @@ if (typeof require !== 'undefined') {
           measure.initBeat(beats, beatType);
         },
                 BeatTypeValueError);
+      });
+    });
+
+    describe("#setBeat", function () {
+      it("increase beats", function () {
+        // Given 
+        var nbNote = 2;
+        var nbRest = 2;
+        var data = getTestData(nbNote, nbRest);
+        var beats = 5;
+        var measure = new Measure(data);
+        var expectedNotes = ["p", "p", "r", "r", "r"];
+
+        // When
+        measure.setBeats(beats, measure.getBeatType());
+
+        // Then
+        var notes = notesToCharTab(data);
+        assert.deepEqual(notes, expectedNotes);
       });
     });
   });
