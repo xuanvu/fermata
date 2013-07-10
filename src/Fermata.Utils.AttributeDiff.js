b6325aa0d89ff891d5a33d601c935149bd35874e
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-10 19:45:42 +0200

    extract processor list into a dedicated file

--- src/Fermata.Utils.AttributeDiff.js
+++ src/Fermata.Utils.AttributeDiff.js
@@ -1,10 +1,6 @@
 (function () {
   "use strict";
 
-  var UpdateBeatType = Fermata.Utils.AttributeDiff.UpdateBeatType;
-  var UpdateBeats = Fermata.Utils.AttributeDiff.UpdateBeats;
-  var UpdateDivisions = Fermata.Utils.AttributeDiff.UpdateDivisions;
-
   Fermata.Utils.AttributeDiff = function (attr1, attr2) {
     this.attr1 = attr1;
     this.attr2 = attr2;
@@ -15,15 +11,9 @@
 
   var AttributeDiff = Fermata.Utils.AttributeDiff;
 
-  var attributesProcessors = [
-    new UpdateBeatType(),
-    new UpdateBeats(),
-    new UpdateDivisions()
-  ];
-
   AttributeDiff.prototype.fillResult = function () {
-    for (var i = 0; i < attributesProcessors.length; i++) {
-      var attributesProcessor = attributesProcessors[i];
+    for (var i = 0; i < this.processorList.length; i++) {
+      var attributesProcessor = this.processorList[i];
 
       if (attributesProcessor.canProcess(this.delta)) {
         attributesProcessor.process(this.attr1, this.attr2, this.delta, this.result);
