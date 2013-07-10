fd7077c4aa488c54e0b455b49ff582f71cdd6975
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-10 15:14:58 +0200

    using parent methods

--- src/Fermata.Utils.AttributeDiff.UpdateDivisions.js
+++ src/Fermata.Utils.AttributeDiff.UpdateDivisions.js
@@ -12,24 +12,4 @@
   UpdateDivisions.prototype = new UpdateBase();
   UpdateDivisions.constructor = UpdateDivisions;
 
-  UpdateDivisions.prototype.canProcess = function (delta) {
-    return this.pathExists(delta);
-  };
-
-  UpdateDivisions.prototype.pathExists = function (obj) {
-    for (var i = 0; i < this.path.length; i++) {
-      var pathElement = this.path[i];
-      if (typeof obj[pathElement] !== "undefined") {
-        obj = obj[pathElement];
-      } else {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  UpdateDivisions.prototype.process = function (attr1, attr2, delta, result) {
-    result.divisions = attr2.divisions;
-  };
-
 }).call(this);
