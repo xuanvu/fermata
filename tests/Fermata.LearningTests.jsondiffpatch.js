fd568006633c3bdb320b717f1fd6e1997ecd6ead
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-10 16:59:47 +0200

    add learning test for beat

--- tests/Fermata.LearningTests.jsondiffpatch.js
+++ tests/Fermata.LearningTests.jsondiffpatch.js
@@ -2,7 +2,6 @@ if (typeof require !== 'undefined') {
   var Fermata,
           fs = require('fs'),
           assert = require('assert');
-
   // Use Scons build version or devel version (using node vm)
   if (fs.existsSync(__dirname + '/../build/fermata/fermata.node.js')) {
     Fermata = require('..');
@@ -12,16 +11,15 @@ if (typeof require !== 'undefined') {
             '/Fermata.Dev.Node.js');
   }
 
-  // Test utils
+// Test utils
   Fermata.Tests = require('./Fermata.Tests.Utils.js').Tests;
 }
 
 (function () {
-  
+
   var defaultDivisions = 1;
   var defaultBeats = 4;
   var defaultBeatType = 4;
-
   var createAttributes = function () {
     return {
       divisions: defaultDivisions,
@@ -31,7 +29,6 @@ if (typeof require !== 'undefined') {
       }
     };
   };
-
   describe("Learning test - jsondiffpatch", function () {
     describe("#diff", function () {
       it("divisions", function () {
@@ -41,13 +38,32 @@ if (typeof require !== 'undefined') {
         var attributes1 = createAttributes();
         var attributes2 = createAttributes();
         attributes2.divisions = divisions2;
-
         var expectedDelta = {
           divisions: [divisions1, divisions2]
         };
 
         // When
         var delta = jsondiffpatch.diff(attribute1, attribute2);
+
+        // Then
+        assert.deepEqual(delta, expectedDelta);
+      });
+
+      it("beats", function () {
+        // Given 
+        var beats1 = defaultBeats;
+        var beats2 = 2;
+        var attributes1 = createAttributes();
+        var attributes2 = createAttributes();
+        attributes2.time.beats = beats2;
+        var expectedDelta = {
+          time: {
+            beats: [beats1, beats2]
+          }
+        };
+
+        // When
+        var delta = jsondiffpatch.diff(attributes1, attributes2);
 
         // Then
         assert.deepEqual(delta, expectedDelta);
