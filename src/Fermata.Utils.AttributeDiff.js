477fb3ba10c640dedb04aa0f5b7fe45dacce01bf
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-10 17:32:21 +0200

    code for AttriuteDiff

--- src/Fermata.Utils.AttributeDiff.js
+++ src/Fermata.Utils.AttributeDiff.js
@@ -1,6 +1,10 @@
 (function () {
   "use strict";
 
+  var UpdateBeatType = Fermata.Utils.AttributeDiff.UpdateBeatType;
+  var UpdateBeats = Fermata.Utils.AttributeDiff.UpdateBeats;
+  var UpdateDivisions = Fermata.Utils.AttributeDiff.UpdateDivisions;
+
   Fermata.Utils.AttributeDiff = function (attr1, attr2) {
     this.attr1 = attr1;
     this.attr2 = attr2;
@@ -11,12 +15,24 @@
 
   var AttributeDiff = Fermata.Utils.AttributeDiff;
 
-  AttributeDiff.prototype.fillResult = function () {
+  var attributesProcessors = [
+    new UpdateBeatType(),
+    new UpdateBeats(),
+    new UpdateDivisions
+  ];
 
+  AttributeDiff.prototype.fillResult = function () {
+    for (var i = 0; i < attributesProcessors.length; i++) {
+      var attributesProcessor = attributesProcessors[i];
+
+      if (attributesProcessor.canProcess(this.delta)) {
+        attributesProcessor.process(this.attr1, this.attr2, this.delta, this.result);
+      }
+    }
   };
 
   AttributeDiff.prototype.getResult = function () {
-
+    return this.result;
   };
 
 }).call(this);
