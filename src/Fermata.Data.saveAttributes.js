1636f7dddf15d33abf8d309a373717aaa69fc4f4
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-12 13:21:31 +0200

    finish code for saveAttributes

--- src/Fermata.Data.saveAttributes.js
+++ src/Fermata.Data.saveAttributes.js
@@ -1,6 +1,8 @@
 (function () {
   "use strict";
 
+  var Measure = Fermata.Data.Measure;
+
   var Data = Fermata.Data;
 
   Data.prototype.saveAttributes = function ()
@@ -8,10 +10,28 @@
     this.forEachPart(function (part) {
       var measures = part.measure;
       var previousMeasure = null;
-      for (var i = 0 ; i < measures.length ; i++) {
+      for (var i = 0; i < measures.length; i++) {
         var measure = measures[i];
-        
+        if (i === 0) {
+          saveAttributesFirst(measure);
+        } else {
+          saveAttributesOther(previousMeasure, measure);
+        }
+        previousMeasure = measure;
       }
     });
   };
+
+  var saveAttributesFirst = function (measureData) {
+    var measure = new Measure(measureData);
+
+    measure.updateAttributes();
+  };
+
+  var saveAttributesOther = function (previousMeasure, measureData) {
+    var measure = new Measure(measureData);
+    
+    measure.updateFromPrevious(previousMeasure);
+  };
+
 }).call(this);
