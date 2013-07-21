9f27efd2bc23fedcf31ed235e327c1fa51bf09c3
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-21 15:18:13 +0200

    format file

--- src/Fermata.Utils.js
+++ src/Fermata.Utils.js
@@ -1,9 +1,10 @@
 (function () {
-	"use strict";
+  "use strict";
 
   Fermata.Utils = {};
   Fermata.Utils.Clone = function (obj) {
-    var newObj = (Object.prototype.toString.call(obj) === '[object Array]') ? [] : {};
+    var newObj = (Object.prototype.toString.call(obj) ===
+            '[object Array]') ? [] : {};
     for (var i in obj) {
       if (obj.hasOwnProperty(i)) {
         if (obj[i] && typeof obj[i] === "object") {
@@ -18,10 +19,11 @@
   };
 
   Fermata.Utils.CloneEpure$fermata = function (obj) {
-    var newObj = (Object.prototype.toString.call(obj) === '[object Array]') ? [] : {};
+    var newObj = (Object.prototype.toString.call(obj) ===
+            '[object Array]') ? [] : {};
     for (var i in obj) {
       if (obj.hasOwnProperty(i) &&
-            i !== "$fermata") {
+              i !== "$fermata") {
         if (obj[i] && typeof obj[i] === "object") {
           newObj[i] = Fermata.Utils.Clone(obj[i]);
         }
@@ -36,7 +38,7 @@
   // Delete ?
   Fermata.Utils.FirstLast = function (obj)
   {
-    return ({ first: 0, last: obj.length - 1 });
+    return ({first: 0, last: obj.length - 1});
   };
 
   Fermata.Utils.minusToCamelCase = function (str) {
