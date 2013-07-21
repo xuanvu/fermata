d1979d3163c09cd8a3230ab49d93d1f78c3cf9e4
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-07-21 15:17:30 +0200

    code for clone without $fermata

--- src/Fermata.Utils.js
+++ src/Fermata.Utils.js
@@ -17,6 +17,22 @@
     return newObj;
   };
 
+  Fermata.Utils.CloneEpure$fermata = function (obj) {
+    var newObj = (Object.prototype.toString.call(obj) === '[object Array]') ? [] : {};
+    for (var i in obj) {
+      if (obj.hasOwnProperty(i) &&
+            i !== "$fermata") {
+        if (obj[i] && typeof obj[i] === "object") {
+          newObj[i] = Fermata.Utils.Clone(obj[i]);
+        }
+        else {
+          newObj[i] = obj[i];
+        }
+      }
+    }
+    return newObj;
+  };
+
   // Delete ?
   Fermata.Utils.FirstLast = function (obj)
   {
